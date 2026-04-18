1e2eaf6f3d8bebd3c2d73757ea676aeb856c6ef2
balaji50 <balajibb1812790@gmail.com>
2026-04-18 16:14:23 +0530

    Create sidebar, navebar, dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Layout } from './core/layout/layout/layout';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard 
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ] 
+    }
+];
